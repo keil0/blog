4b8684943a4db4b84e5f00f3fa53c01af0d3024b
Pierre Cruz <contact@pierrecruz.com>
2020-03-20 20:55:22 +0100

    Start #127

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,10 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={10}>{children}</BlogContext.Provider>;
+  const blogPosts = [{ title: "Blog post #1" }, { title: "Blog post #2" }];
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
   return (
     <View>
-      <Text>Index Screen {value}</Text>
+      <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={post => post.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
     </View>
   );
 };
