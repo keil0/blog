ffda215204e6b66ba9af56a5232811dbbdcd7511
Pierre Cruz <contact@pierrecruz.com>
2020-03-20 20:50:40 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import { BlogProvider } from "./src/context/BlogContext";
+
+import IndexScreen from "./src/screens/indexScreen";
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs"
+    }
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
